5eefd8734e3aee939403cab888bdf7f706db08f5
CharlieCrossland <cro24003689@heartofyorkshire.ac.uk>
2025-03-28 13:50:47 +0000

    Added starter tilemaps

--- FMP-Year1/Assets/Scripts/CameraController.cs
+++ FMP-Year1/Assets/Scripts/CameraController.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     // Zoom Variables
-    private float zoom;
-    private float originalSize;
-    private float zoomMultiplier = 0.01f;
-    private float minZoom;
-    private float maxZoom = 8.5f;
-    private float velocity = 0f;
-    private float smoothTime = 0.15f;
+    // private float zoom;
+    // private float originalSize;
+    // private float zoomMultiplier = 0.01f;
+    // private float minZoom;
+    // private float maxZoom = 8.5f;
+    // private float velocity = 0f;
+    // private float smoothTime = 0.15f;
 
 
     [Header("Camera")]
@@ -23,11 +23,11 @@ public class CameraController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        originalSize = cam.orthographicSize;
-        zoom = cam.orthographicSize;
+        // originalSize = cam.orthographicSize;
+        // zoom = cam.orthographicSize;
 
-        maxZoom = originalSize;
-        minZoom = originalSize - 0.35f;
+        // maxZoom = originalSize;
+        // minZoom = originalSize - 0.35f;
     }
 
     // FixedUpdate is better for camera to look smooth
@@ -38,19 +38,19 @@ public class CameraController : MonoBehaviour
         transform.position = Vector3.Lerp(transform.position, Maintarget.position + offset, followSpeed * Time.deltaTime);
 
 
-        if (Input.GetKey(KeyCode.Mouse0))
-        {
-            // zoom camera in
-            zoom -= zoomMultiplier;
-            zoom = Mathf.Clamp(zoom, minZoom, maxZoom);
-            cam.orthographicSize = Mathf.SmoothDamp(cam.orthographicSize, zoom, ref velocity, smoothTime);
-        }
-        else //zoom back out
-        {
-            zoom += zoomMultiplier;
-            zoom = Mathf.Clamp(zoom, minZoom, maxZoom);
-            cam.orthographicSize = Mathf.SmoothDamp(cam.orthographicSize, zoom, ref velocity, smoothTime);
-        }
+        // if (Input.GetKey(KeyCode.Mouse0))
+        // {
+        //     // zoom camera in
+        //     zoom -= zoomMultiplier;
+        //     zoom = Mathf.Clamp(zoom, minZoom, maxZoom);
+        //     cam.orthographicSize = Mathf.SmoothDamp(cam.orthographicSize, zoom, ref velocity, smoothTime);
+        // }
+        // else //zoom back out
+        // {
+        //     zoom += zoomMultiplier;
+        //     zoom = Mathf.Clamp(zoom, minZoom, maxZoom);
+        //     cam.orthographicSize = Mathf.SmoothDamp(cam.orthographicSize, zoom, ref velocity, smoothTime);
+        // }
 
     }
 }
