413b68bdd67943c5cdf8e7dc3dbe9041d2554e4e
CharlieCrossland <cro24003689@heartofyorkshire.ac.uk>
2025-05-15 16:38:29 +0100

    Bug Fix

--- FMP-Year1/Assets/Scripts/WinScreen.cs
+++ FMP-Year1/Assets/Scripts/WinScreen.cs
@@ -8,7 +8,7 @@ public class WinScreen : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.visible = true;
     }
 
     // Update is called once per frame
